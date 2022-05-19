9cd65cf8cfdb9d55896b06206507ec36783043b9
Sreemanreddy <105593450+professional-fox@users.noreply.github.com>
2022-05-19 14:50:27 +0530

    Create Phoneinfoga.py

---
+++ Phoneinfoga.py
@@ -0,0 +1,16 @@
+$ pkg install python
+$ pkg install python2
+$ pkg install python3
+$ pkg install git
+$ python --version
+  (To check version of python)
+$ git clone https://github.com/abhinavkavuri/phoneInfoga
+$ cd phoneInfoga
+$ ls
+$ mv config.example.py
+$ python3 -m pip install -r requirements.txt
+$ ls
+$ python3 phoneinfoga.py -n 91(set any phone number)
+
+
+    ..........© Copyright 2022-2025 ..........
